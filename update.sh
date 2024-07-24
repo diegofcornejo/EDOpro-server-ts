039c53b917d2414b4bf2063ea0152c238a72f057
Diego Cornejo <diegof.cornejo@gmail.com>
2024-07-24 14:16:45 -0600

    chore: modify update script

--- update.sh
+++ update.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 ORG_PATH="/orgs/evolutionygo"
-REPO_NAME="EDOpro-server-ts"
+REPO_NAME="hot-db-update"
 DOCKER_IMAGE_NAME="evolutionygo/server"
-DOCKER_CONTAINER_NAME="evolutionygo-server"
+DOCKER_IMAGE_TAG="beta"
+DOCKER_CONTAINER_NAME="evolutionygo-server-beta"
 LOKI_URL="http://localhost:3100/loki/api/v1/push"
 
 # Go to the repository
@@ -19,7 +20,7 @@ git submodule update --remote
 # Delete running container and image if they exist
 docker stop $DOCKER_CONTAINER_NAME
 docker rm -f $DOCKER_CONTAINER_NAME
-docker rmi $DOCKER_IMAGE_NAME:latest
+docker rmi $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG
 
 # Build the image
 docker build -t $DOCKER_IMAGE_NAME --progress=plain . 2>&1 | tee ../docker-build.log
@@ -35,7 +36,7 @@ docker run -d --env-file ./evolution-server.env \
 -p 7911:7911 \
 -p 7922:7922 \
 -v `pwd`/certs:/app/certs \
--v `pwd`/databases/BabelCDB:/app/databases/evolution:ro \
+-v `pwd`/$REPO_NAME/db/BabelCDB:/app/databases/evolution:ro \
 --log-driver=loki \
 --log-opt loki-url=$LOKI_URL \
-$DOCKER_IMAGE_NAME:latest
+$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG
